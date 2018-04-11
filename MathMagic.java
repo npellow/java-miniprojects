6f13a3d28f91c93859a7f577815a08b8e73355d7
Nichole Pellow <mrspellow@comcast.net>
2018-04-11 11:15:13 -0500

    Math Magic added
    First project from the Learn Java course

---
+++ MathMagic.java
@@ -0,0 +1,14 @@
+public class MathMagic {
+    public static void main(String[] args) {
+
+        int myNumber = 17;
+        int stepOne = myNumber * myNumber;
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo / myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour / myNumber;
+        int stepSix = stepFive / 1;
+
+        System.out.println(stepSix);
+    }
+}
